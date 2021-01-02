2f0480626b910cba47c3aca2fca8acc4e55f3473
user name <user email>
2021-01-02 13:57:32 +0530

    okay

--- db/db/settings.py
+++ db/db/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '&cj$%9ice^ba1*+j^4gg7ik^r&_)-7v*2byd1g#613swdf%peh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','https://testforawanish.herokuapp.com/']
 
 
 # Application definition
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- db/db/urls.py
+++ db/db/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import LoginView,LogoutView
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
+
 urlpatterns = [
 
     path('admin/', admin.site.urls),
